991d60b827acfd9bfb568c0b17bb30dd42535ce6
Rahul Kumar <rahulrai2822march@gmail.com>
2023-05-24 14:06:31 +0530

    first commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        We welcome the people of town to our exclusive Pizza shop with the bottom of our heart.<br/>It will be our honour to serve you'll with our best.
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
